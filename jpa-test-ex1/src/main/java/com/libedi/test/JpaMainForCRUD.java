86138826eec39665875c9d5ba0d949b59f8e4c11
libedi <libedi@gmail.com>
2015-08-11 23:00:59 +0900

    Modify Class name

--- jpa-test-ex1/src/main/java/com/libedi/test/JpaMainForCRUD.java
+++ jpa-test-ex1/src/main/java/com/libedi/test/JpaMainForCRUD.java
@@ -7,7 +7,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
-public class JpaMain {
+public class JpaMainForCRUD {
 
 	public static void main(String[] args) {
 		// 엔티티 매니저 팩토리 생성
--- jpa-test-ex1/src/main/java/com/libedi/test/JpaMainForEntityReg.java
+++ jpa-test-ex1/src/main/java/com/libedi/test/JpaMainForEntityReg.java
@@ -5,7 +5,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
-public class JpaMain2 {
+public class JpaMainForEntityReg {
 
 	public static void main(String[] args) {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpatest");
